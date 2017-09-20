0ac1e25adcb59ab2d609402f829163f3899b35da
JCGrant <jamescolin.grant@gmail.com>
2017-09-20 12:33:53 +0100

    Delete dead creeps

--- main.js
+++ main.js
@@ -6,7 +6,17 @@ const roles = {
   'builder': require('role.builder'),
 }
 
+function deleteDeadCreeps() {
+  for (const name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+        delete Memory.creeps[name];
+    }
+  }
+}
+
 module.exports.loop = function () {
+  deleteDeadCreeps();
+
   for (const name in Game.creeps) {
     const creep = Game.creeps[name];
     roles[creep.memory.role](creep);
