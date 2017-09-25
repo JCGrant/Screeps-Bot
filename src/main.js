a8060e085b3c1dc2194ff3eb18706341bbbb2a93
JCGrant <jamescolin.grant@gmail.com>
2017-09-26 00:26:16 +0100

    Add tower code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 'use strict';
 const creepManager = require('creepManager');
 
+const towerHeal = (tower) => {
+  const damagedStructures = tower.room.find(FIND_STRUCTURES,
+    { filter: (structure) => structure.hits < structure.hitsMax });
+  const sortedByRelativeDamage = _.sortBy(damagedStructures, (structure) => {
+    return structure.hits / structure.hitsMax;
+  });
+  const mostRelativelyDamaged = sortedByRelativeDamage[0];
+  tower.repair(mostRelativelyDamaged);
+};
+
+const towerAttack = (tower) => {
+  const closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+  if (closestHostile) {
+    tower.attack(closestHostile);
+  }
+};
+
+const runTower = () => {
+  const tower = Game.getObjectById('59c97cc7d671737894c0da06');
+  towerHeal(tower);
+  towerAttack(tower);
+};
+
 module.exports.loop = () => {
+  runTower();
   creepManager.deleteDeadCreeps();
   creepManager.spawnNewCreeps();
   creepManager.creepsPerformRoles();
